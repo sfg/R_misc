a673c8e7454b7bc5f592163c2ede4e6a5126946e
sfg <sven.garbade@evall.org>
2021-02-01 16:42:22 +0100

    Add files via upload
    FIX: rows with now semicolon are now copied to new data frame

--- misc.R
+++ misc.R
@@ -5,17 +5,21 @@ replicateImpCols <- function(m) {
         cols <- sapply(m[i,], function(x) {
             res <- all(grepl(";", x))
         })
-        if(sum(cols)==0)
-            next
-        ## replicate cols with ';'
-        res <- do.call("cbind", sapply(m[i, cols], function(x) {
-            strsplit(x, ";")
-        }))
-        colnames(res) <- colnames(m)[cols]
-        ## replicate "old" data
-        tmp <- as.data.frame(lapply(m[i,!cols], rep, nrow(res)))
+        if(sum(cols)>0) {
+            ## replicate cols with ';'
+            res <- do.call("cbind", sapply(m[i, cols], function(x) {
+                strsplit(x, ";")
+            }))
+            colnames(res) <- colnames(m)[cols]
+            ## replicate "old" data
+            tmp <- as.data.frame(lapply(m[i,!cols], rep, nrow(res)))
+            tmp <- cbind(res, tmp)
+            tmp <- tmp[colnames(m)]
+        } else {
+            tmp <- m[i,]
+            tmp <- tmp[colnames(m)]
+        }
         ## combine 'old' with replicated data
-        tmp <- cbind(res, tmp)
         vals <- rbind(vals, tmp)
     }
     ## ready
