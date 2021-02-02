e2647df2e05a832c87fd323b4dcb07fd97637f8d
Sven Garbade <sven.garbae@evall.org>
2021-02-02 13:30:58 +0100

    Added new argument 'sep' ti specufy the separator

--- misc.R
+++ misc.R
@@ -1,14 +1,14 @@
-replicateImpCols <- function(m) {
+replicateImpCols <- function(m, sep=";") {
     vals <- list()
     for(i in 1:nrow(m)) {
         ## check columns with ';'
         cols <- sapply(m[i,], function(x) {
-            res <- all(grepl(";", x))
+            res <- all(grepl(sep, x))
         })
         if(sum(cols)>0) {
             ## replicate cols with ';'
             res <- do.call("cbind", sapply(m[i, cols], function(x) {
-                strsplit(x, ";")
+                strsplit(x, sep)
             }))
             colnames(res) <- colnames(m)[cols]
             ## replicate "old" data
